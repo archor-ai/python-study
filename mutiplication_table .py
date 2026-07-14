b74e52ccab06f54285d57927aec40204a3d7f056
Parth <parthom9999@gmail.com>
2026-07-15 00:55:29 +0530

    by the help of this code make any table

---
+++ mutiplication_table .py
@@ -0,0 +1,12 @@
+n = int(input(" enter your table number :- "))
+
+for j in range(11):
+   print(n*j)  #1st case
+
+a = 1
+n = int(input("enter your table number :- "))
+print(n*1)
+
+while a<=9:
+   a+=1
+   print(n*a)    #2nd case
