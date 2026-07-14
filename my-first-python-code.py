d2bcb2126a3faf71837cfe126061c43e3fd41991
Parth <parthom9999@gmail.com>
2026-07-15 01:25:24 +0530

    if number is negative or positive print back number by the help of this code

--- my-first-python-code.py
+++ my-first-python-code.py
@@ -1,1 +1,44 @@
-print("Hello world")
+print("hello world")
+#print("hello@world") #1st case
+
+#  a = input("enter your first word :- ")
+#  b = input("enter your second word :- ")
+ #c = input("enter your third word :- ")
+ #print(a,b,c)
+
+ #print("hello" * 5)
+
+#  a = input("enter your desire word you want to write :- ")
+#  b = int(input("enter your limit how many times to do print :- "))
+
+#  for j in range (b):
+#      print(a)
+
+#   a = input(" enter what you want :- ")
+
+#   for j in range (10):
+#       print(a)
+
+#   a = int(input("enter your number :- "))
+#   b = int(input("enter your number :- "))
+#   c = '&'
+
+#   for d in range(a):
+#       print(a)
+#  print(c)
+
+#  for e in range(b):
+#      print(b)
+
+#  n = int(input(" enter your table number :- "))
+
+#  for j in range(11):
+#      print(n*j)
+
+ a = 1
+ n = int(input("enter your table number :- "))
+ print(n*1)
+
+ while a<=9:
+     a+=1
+     print(n*a)
---
+++ zero_converter.py
@@ -0,0 +1,12 @@
+a = int(input("enter your number :- "))
+print(a)
+while a>=1:
+    a-=1
+    print(a)
+if -a==-a:
+  while a<=-1:
+    a+=1
+    print(a)
+if a==0:
+    print("already zero")
+
