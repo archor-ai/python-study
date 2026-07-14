b132df07f76480a6a13841ec93f638bde26eabd6
Parth <parthom9999@gmail.com>
2026-07-15 00:42:02 +0530

    this code is used for print any word in multipe times

---
+++ multi_printing.py
@@ -0,0 +1,12 @@
+print("hello" * 5)
+
+a = input("enter your desire word you want to write :- ")
+b = int(input("enter your limit how many times to do print :- "))
+
+for j in range (b):
+     print(a)
+
+a = input(" enter what you want :- ")
+
+for j in range (10):
+   print(a)
